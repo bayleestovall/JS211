b1f43c3aa5b682db9559d46a2571686cb752cf6c
bayleestovall <bayleestovall@gmail.com>
2020-10-17 13:58:05 -0500

    cleaned up code

--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -1,4 +1,4 @@
-  //worked with others on discord :) 
+//worked with others on discord :) 
 'use strict';
 
 // brings in the assert module for unit testing
@@ -36,59 +36,56 @@ const printBoard = () => {
 const horizontalWin = () => {
   // Your code here to check for horizontal wins
   if (
-  (board[0][0] == board[0][1] && board[0][0] == board[0][2]) ||
-  (board[1][0] == board[1][1] && board[1][0] == board[1][2]) ||
-  (board[2][0] == board[2][1] && board[2][0] == board[2][2]))
-  {
+    (board[0][0] == board[0][1] && board[0][0] == board[0][2]) ||
+    (board[1][0] == board[1][1] && board[1][0] == board[1][2]) ||
+    (board[2][0] == board[2][1] && board[2][0] == board[2][2])) {
     return true
   }
-  
+
 }
 
 const verticalWin = () => {
   // Your code here to check for vertical wins
   if (
-  (board[0][0] == board[1][0] && board[0][0] == board[2][0]) ||
-  (board[0][1] == board[1][1] && board[1][0] == board[2][1]) ||
-  (board[0][2] == board[1][2] && board[2][0] == board[2][2]))
-  {
+    (board[0][0] == board[1][0] && board[0][0] == board[2][0]) ||
+    (board[0][1] == board[1][1] && board[1][0] == board[2][1]) ||
+    (board[0][2] == board[1][2] && board[2][0] == board[2][2])) {
     return true
   }
-  
+
 }
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
   if (
-  (board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
-  (board[0][2] == board[1][1] && board[0][2] == board[2][0]))
-  {
+    (board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
+    (board[0][2] == board[1][1] && board[0][2] == board[2][0])) {
     return true
   }
 }
 
 const checkForWin = () => {
   // Your code here call each of the check for types of wins
-  if (horizontalWin() || verticalWin() || diagonalWin()){
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
     return true
   }
 }
 
 const ticTacToe = (row, column) => {
   // Your code here to place a marker on the board
-if (board[row][column] == " "){
-  board[row][column] = playerTurn;
-  if (playerTurn == "X"){
-    playerTurn = "O"
+  if (board[row][column] == " ") {
+    board[row][column] = playerTurn;
+    if (playerTurn == "X") {
+      playerTurn = "O"
+    } else {
+      playerTurn = "X"
+    }
   } else {
-    playerTurn = "X"
+    console.log("Looks like this space is taken, try another!");
   }
-} else {
-  console.log("Looks like this space is taken, try another!");
-}
 
   // then check for a win
-  if (checkForWin() == true){
+  if (checkForWin() == true) {
     console.log("Game Over!" + playerTurn + "is the champion!")
   }
 }
@@ -113,22 +110,22 @@ if (typeof describe === 'function') {
   describe('#ticTacToe()', () => {
     it('should place mark on the board', () => {
       ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepEqual(board, [[' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']]);
     });
     it('should alternate between players', () => {
       ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepEqual(board, [['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' ']]);
     });
     it('should check for vertical wins', () => {
-      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
+      board = [[' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' ']];
       assert.equal(verticalWin(), true);
     });
     it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+      board = [['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' ']];
       assert.equal(horizontalWin(), true);
     });
     it('should check for diagonal wins', () => {
-      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
+      board = [['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X']];
       assert.equal(diagonalWin(), true);
     });
     it('should detect a win', () => {
